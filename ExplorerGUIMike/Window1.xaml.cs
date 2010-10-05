28e876dacf2a829457ef4fa0eb14c1a1ec980bb8
Jimmy Warren <jdwarren21@tntech.edu>
2010-10-05 16:08:45 -0400

    *Seperate GUI
    Signed-off-by: Jimmy Warren <jdwarren21@tntech.edu>

---
+++ ExplorerGUIMike/Window1.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using libSMARTMultiTouch.Table;
+using libSMARTMultiTouch.Controls;
+
+using ExplorerLib;
+using ExplorerLib.ContentTypes;
+
+namespace MultitouchExplorer
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+            TableManager.Initialize(this, LayoutRoot);
+            try
+            {
+                Explorer explorer = new Explorer("../../../Sample.xml");
+                ExplorerContentMap rootMap = explorer.getRootContentMap();
+                InteractiveBorder interactiveBorder = new InteractiveBorder();
+                interactiveBorder.Child = rootMap.getImage();
+
+                LayoutRoot.Children.Add(interactiveBorder);
+
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("Got exception: " + e);
+            }
+
+            LayoutRoot.Children.Add(new TableControl());
+            TableManager.IsFullScreen = false;
+
+           
+
+
+            
+        }
+    }
+}
