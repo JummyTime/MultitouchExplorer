8c98fe99f72cbfd86e9031b643ecc568ca6e1ecb
Jimmy Warren <jdwarren21@tntech.edu>
2010-10-06 22:15:18 -0400

    Added ExplorerClickBehavior and IDProperties
    Signed-off-by: Jimmy Warren <jdwarren21@tntech.edu>

---
+++ ExplorerGUIJimmy/ExplorerClickBehavior.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Interactivity;
+using System.Windows.Controls;
+
+using libSMARTMultiTouch;
+using libSMARTMultiTouch.Controls;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Input;
+using libSMARTMultiTouch.Input;
+
+using ExplorerLib.ContentTypes;
+using ExplorerLib.Exceptions;
+
+namespace ExplorerLib
+{
+    public class ClickBehavior : Behavior<InteractiveFrameworkElement>
+    {
+        private FrameworkElement m_element;
+        //private Dictionary<int, IDProperties> dictIDProperties = new Dictionary<int, IDProperties>();
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            m_element = base.AssociatedObject.FrameworkElement;
+            ((InteractiveBorder)m_element).StylusButtonDown += new StylusButtonEventHandler(Point_Touch_Confirm);
+            //((InteractiveBorder)m_element).TouchDown += new TouchContactEventHandler(Point_TouchUp_Confirm);
+        }
+
+        public void Point_Touch_Confirm(object sender, EventArgs e)
+        {
+            /*Point point = new Point();
+            bool IsStylusCaptured = ((InteractiveBorder)m_element).IsStylusCaptured;
+            IDProperties idProperties = this.GetID(((TouchContactEventArgs)e).TouchContact.ID);
+            point = ((TouchContactEventArgs)e).TouchContact.GetPosition((InteractiveBorder)sender);
+            Console.Write(point.X);
+            Console.Write(point.Y);
+            Console.Write(point.ToString());
+            System.Console.WriteLine("Normal is the Watchword");*/
+        }
+       /* public void Point_TouchDown_Confirm(object sender, EventArgs e)
+        {
+
+        }*/
+
+        protected override void OnDetaching()
+        {
+
+        }
+        #region Utilities
+
+        // Given a mouse ID:
+        // - create a dictionary entry storing IDProperties by mouse ID if we don't already have one
+        // - return the IDProperties object that stores status information associated with that ID
+       /* private IDProperties GetID(int mouseID)
+        {
+            // If we have the mouseID, return the corrected one
+            if (dictIDProperties.ContainsKey(mouseID))
+            {
+                return dictIDProperties[mouseID];
+            }
+            else
+            {
+                // We don't have it. Add a new ID Property then return it
+                IDProperties idProperties = new IDProperties(mouseID);
+                dictIDProperties.Add(mouseID, idProperties);
+                return idProperties;
+            }
+        }*/
+        #endregion
+
+    }
+
+}
---
+++ ExplorerGUIJimmy/IDProperties.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace TouchApplication
+{
+    // Associates some basic properties with an ID:
+    // -Color
+    // -Brush
+    // -ID
+    // You can easily extend this class to add other properties (e.g., the mouse-owner's name), or to adjust the defaults
+
+    class IDProperties
+    {
+        private Color myColor;
+        private Brush myBrush;
+        private int myID;
+
+        // Constructor. Set a few defaults
+        public IDProperties (int ID)
+        {
+            this.ID = ID;
+            this.Color = Colors.Orange;
+            this.Brush = Brushes.Orange;
+        }
+
+        public int ID
+        {
+            get { return myID; }
+            set { myID = this.GetCorrectedID(value); }
+        }
+        
+        public Color Color 
+        { 
+            get { return myColor; } 
+            set { myColor = value; } 
+        }
+
+        public Brush Brush
+        {
+            get { return myBrush; }
+            set { myBrush = value; }
+        }
+
+        #region Static Utilities
+        private static int idCounter = 0;
+        private static Dictionary<int, int> dictIDs = new Dictionary<int, int>();        // Given an mouse ID, find and record its equivalent zero-based ID (or return it if we have it)
+        // The emulator does not guarantee that mice will be numbered from 0, 1, etc. 
+        // This corrects the ID to always make it zero-based, where each new ID gets the next number
+        private int GetCorrectedID(int mouseID)
+        {
+            // If we have the mouseID, return the corrected one
+            if (IDProperties.dictIDs.ContainsKey(mouseID))
+            {
+                return IDProperties.dictIDs[mouseID];
+            }
+            else
+            {
+                // We don't have it. Add it, where the corrected ID is the current
+                // value of the mouseCounter (which was initially 0) then return it
+                IDProperties.dictIDs.Add(mouseID, IDProperties.idCounter);
+                IDProperties.idCounter++;
+                return IDProperties.dictIDs[mouseID];
+            }
+        }
+        #endregion
+    }
+}
